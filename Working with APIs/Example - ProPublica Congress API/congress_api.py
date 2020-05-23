0506c47b3df6aa4c53de31c97875d9868dbbe1de
Dan Bressner <41024896+danbressner@users.noreply.github.com>
2020-05-23 15:05:11 -0400

    added function for party alignment check

--- Working with APIs/Example - ProPublica Congress API/congress_api.py
+++ Working with APIs/Example - ProPublica Congress API/congress_api.py
@@ -6,14 +6,17 @@ HouseVote = namedtuple('HouseVote', 'congress chamber session roll_call source u
                                     'question_text description vote_type date time result democratic republican '
                                     'independent total')
 
+HouseMember = namedtuple('HouseMember', 'id api_uri name party state district total_votes votes_with_party '
+                                        'party_votes_pct rank notes')
+
 api_key = 'yourkeyhere'
 
 
 def get_recent_house_votes() -> List[HouseVote]:
     """Fetch recent house votes and store them as namedtuples"""
     url = f'https://api.propublica.org/congress/v1/house/votes/recent.json'
-    headers = {'Accept':'application/json',
-               'X-API-Key':api_key}
+    headers = {'Accept': 'application/json',
+               'X-API-Key': api_key}
     resp = requests.get(url, headers=headers)
     resp.raise_for_status()
     results = resp.json()['results']['votes']
@@ -22,3 +25,19 @@ def get_recent_house_votes() -> List[HouseVote]:
         votes.append(HouseVote(**r))
 
     return votes
+
+
+def get_house_alignment() -> List[HouseMember]:
+    """Fetch voting record by party alignment and store as namedtuples for each member"""
+    url = 'https://api.propublica.org/congress/v1/116/house/votes/party.json'
+    headers = {'Accept': 'application/json',
+               'X-API-Key': api_key}
+    resp = requests.get(url, headers=headers)
+    resp.raise_for_status()
+    results = resp.json()['results'][0]['members']
+    voting_records = []
+
+    for r in results:
+        voting_records.append(HouseMember(**r))
+
+    return sorted(voting_records, key=lambda x: x.party_votes_pct, reverse=True)
--- Working with APIs/Example - ProPublica Congress API/program.py
+++ Working with APIs/Example - ProPublica Congress API/program.py
@@ -3,13 +3,20 @@ from pprint import pprint
 
 
 def main():
-    results = congress_api.get_recent_house_votes()
-    print(f'Identified {len(results)} recent House votes.')
+    recent_votes = congress_api.get_recent_house_votes()
+    print(f'Identified {len(recent_votes)} recent House votes.')
     print()
-    for r in results:
+    for r in recent_votes:
         pprint(f"Date: {r.date} -  {r.bill['title']}")
-        pprint(r.result)
         print()
+        pprint(f"Latest action: {r.bill['latest_action']}")
+        pprint(f'Result: {r.result}')
+        print('-' * 100)
+
+    print('The 10 House members who vote along party lines the most:')
+    alignment = congress_api.get_house_alignment()
+    for idx, member in enumerate(alignment[:10], 1):
+        print(f'{idx}. {member.name} - {member.party} {member.party_votes_pct}')
 
 
 if __name__ == '__main__':
