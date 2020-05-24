acbfca3362a1c999245741a30f179ff268ff55cc
Dan Bressner <41024896+danbressner@users.noreply.github.com>
2020-05-23 21:02:10 -0400

    added function for keyword searches

--- Working with APIs/Example - ProPublica Congress API/congress_api.py
+++ Working with APIs/Example - ProPublica Congress API/congress_api.py
@@ -9,11 +9,14 @@ HouseVote = namedtuple('HouseVote', 'congress chamber session roll_call source u
 HouseMember = namedtuple('HouseMember', 'id api_uri name party state district total_votes votes_with_party '
                                         'party_votes_pct rank notes')
 
+Statement = namedtuple('Statement', 'url date title statement_type member_id congress member_uri name chamber state '
+                                    'party subjects')
+
 api_key = 'yourkeyhere'
 
 
 def get_recent_house_votes() -> List[HouseVote]:
-    """Fetch recent house votes and store them as namedtuples"""
+    """Fetch recent house votes and store as a list of namedtuples"""
     url = f'https://api.propublica.org/congress/v1/house/votes/recent.json'
     headers = {'Accept': 'application/json',
                'X-API-Key': api_key}
@@ -28,7 +31,7 @@ def get_recent_house_votes() -> List[HouseVote]:
 
 
 def get_house_alignment() -> List[HouseMember]:
-    """Fetch voting record by party alignment and store as namedtuples for each member"""
+    """Fetch voting record stats for each member and store as a list of namedtuples"""
     url = 'https://api.propublica.org/congress/v1/116/house/votes/party.json'
     headers = {'Accept': 'application/json',
                'X-API-Key': api_key}
@@ -41,3 +44,19 @@ def get_house_alignment() -> List[HouseMember]:
         voting_records.append(HouseMember(**r))
 
     return sorted(voting_records, key=lambda x: x.party_votes_pct, reverse=True)
+
+
+def get_statements(keyword) -> List[Statement]:
+    """Fetch recent Congressional statements on a specific topic and store results as a list of namedtuples"""
+    url = f'https://api.propublica.org/congress/v1/statements/search.json?query={keyword}'
+    headers = {'Accept': 'application/json',
+               'X-API-Key': api_key}
+    resp = requests.get(url, headers=headers)
+    resp.raise_for_status()
+    statements = []
+    results = resp.json()['results']
+
+    for r in results:
+        statements.append(Statement(**r))
+
+    return statements
