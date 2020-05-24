700e0171b730230679078d4e7b97d9cdf04f0664
Dan Bressner <41024896+danbressner@users.noreply.github.com>
2020-05-23 21:00:46 -0400

    updating program for keyword searches

--- Working with APIs/Example - ProPublica Congress API/program.py
+++ Working with APIs/Example - ProPublica Congress API/program.py
@@ -4,13 +4,14 @@ from pprint import pprint
 
 def main():
     recent_votes = congress_api.get_recent_house_votes()
-    print(f'Identified {len(recent_votes)} recent House votes.')
+    print(f'Identified {len(recent_votes)} recent House votes. Printing the most recent 5: ')
     print()
-    for r in recent_votes:
-        pprint(f"Date: {r.date} -  {r.bill['title']}")
+    for idx, vote in enumerate(recent_votes[:5], 1):
+        pprint(f"{idx}. Date: {vote.date} -  {vote.bill['title']}")
         print()
-        pprint(f"Latest action: {r.bill['latest_action']}")
-        pprint(f'Result: {r.result}')
+        pprint(f"Latest action: {vote.bill['latest_action']}")
+        print()
+        pprint(f'Result: {vote.result}')
         print('-' * 100)
 
     print('The 10 House members who vote along party lines the most:')
@@ -18,6 +19,16 @@ def main():
     for idx, member in enumerate(alignment[:10], 1):
         print(f'{idx}. {member.name} - {member.party} {member.party_votes_pct}')
 
+    print()
+    print('-'*100)
+    keyword = input('Enter a search term to see recent statements: ')
+    statements = congress_api.get_statements(keyword)
+    for statement in statements:
+        print(f'{statement.date}: {statement.name} {statement.party}, {statement.state} - Title: {statement.title}')
+        print(statement.url)
+        print('-'*100)
+
 
 if __name__ == '__main__':
     main()
+
